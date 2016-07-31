013d4f78fe5adb0207b80b19bd9edf2c488aa152
aristekrat <aristekrat@gmail.com>
2016-07-31 16:17:12 -0600

    Upgrading to Cedar-14

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 'use strict';
 /***
  * Module dependencies.
-***/
+ ***/
 
 var express = require('express'),
     app = express(),
@@ -18,7 +18,7 @@ var express = require('express'),
 
 var port = process.env.PORT || 3000;
 
-app.configure(function () {
+app.configure(function() {
     app.use(express.compress());
     app.use(express.bodyParser());
     app.use(expressValidator());
@@ -26,22 +26,27 @@ app.configure(function () {
     app.use(express.static(__dirname + '/public'));
 });
 
-app.configure('development', function () {
+app.configure('development', function() {
     dbURL = 'mongodb://localhost/bishenwall';
     mongoose.connect(dbURL);
     //mongoose.set('debug', true);
-    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+    app.use(express.errorHandler({
+        dumpExceptions: true,
+        showStack: true
+    }));
     app.use(express.static(__dirname + '/public'));
     app.use(express.cookieParser());
 });
 
-app.configure('production', function () {
+app.configure('production', function() {
     var username = process.env.MONGO_USERNAME,
         password = process.env.MONGO_PASSWORD;
-    dbURL = 'mongodb://' + username + ':' + password + '@ds053708.mongolab.com:53708/heroku_app20090225';
+    dbURL = 'mongodb://' + username + ':' + password + '@ds053708.mlab.com:53708/heroku_app20090225';
     mongoose.connect(dbURL);
     app.use(express.errorHandler());
-    app.use(express.static(__dirname + '/public', { maxAge: 9874567}));
+    app.use(express.static(__dirname + '/public', {
+        maxAge: 9874567
+    }));
 });
 
 // Routes
@@ -53,6 +58,6 @@ app.get('*', index.getDefault);
 app.post('/comment', index.addComment);
 app.post('/spam', index.reportSpam);
 
-app.listen(port, function () {
+app.listen(port, function() {
     console.log("Express server listening on port 3000");
 });
