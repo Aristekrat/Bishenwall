6977b0ac2b36e3bc2c8a9be8689ee31810ae1ab9
aristekrat <aristekrat@gmail.com>
2016-07-31 22:08:38 -0600

    Getting rid of a Schema error

--- server/schemas/commentSchema.js
+++ server/schemas/commentSchema.js
@@ -1,30 +1,30 @@
 var mongoose = require('../../node_modules/mongoose'),
-	Schema = mongoose.Schema, 
-	ObjectId = Schema.ObjectId; 
+    Schema = mongoose.Schema,
+    ObjectId = Schema.ObjectId;
 
 var commentSchema = new Schema({
-	comment: ObjectId,
-	title: {
-		type: String,
-		required: true
-	},
-	text: {
-		type: String,
-		required: true
-	},
-	date: {
-		type: Date,
-		default: Date.now
-	},
-	spamCount: {
-		type: Number,
-		default: 0
-	},
-	image: {
-		type: Buffer
-	},
-	reply: [commentSchema]
+    comment: ObjectId,
+    title: {
+        type: String,
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    spamCount: {
+        type: Number,
+        default: 0
+    },
+    image: {
+        type: Buffer
+    },
+    reply: String
 });
 
 var commentModel = mongoose.model('comment', commentSchema);
-module.exports = commentModel; 
+module.exports = commentModel;
